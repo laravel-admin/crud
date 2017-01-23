9990db095f3e6def586ae78b8a81fb9d01995815
Rowdy Klijnsmit <rowdy.klijnsmit@in10.nl>
2017-01-23 17:28:20 +0100

    Template changes

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -1,28 +1,24 @@
 @extends('admin::master')
 
 @section('content')
-
-<div class="container">
-
-	<div class="col-md-8 col-md-push-2">
-
-		<table class="table">
-			<thead>
-				<tr>
-					@foreach ($fields->headings() as $heading) <th>	{{ $heading }} </th> @endforeach
-				</tr>
-			</thead>
-			<tbody>
-			@foreach ($records as $record)
-				<tr>
-					@foreach ($fields->values($record) as $item) <td>	{{ $item }} </td> @endforeach
-				</tr>
-			@endforeach
-			</tbody>
-		</table>
-
-	</div>
-
-</div>
-
+	<main>
+		<div class="container-fluid">
+			<div class="col-md-8">
+				<table class="table">
+					<thead>
+						<tr>
+							@foreach ($fields->headings() as $heading) <th>	{{ $heading }} </th> @endforeach
+						</tr>
+					</thead>
+					<tbody>
+					@foreach ($records as $record)
+						<tr>
+							@foreach ($fields->values($record) as $item) <td>	{{ $item }} </td> @endforeach
+						</tr>
+					@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</main>
 @stop

0cf6461f9fb90df63ab0986ca8da9e331a0b4499
Marco Boom <marco@quest-media.nl>
2017-01-24 11:10:45 +0100

    Optimise index template

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -4,20 +4,34 @@
 
 <div class="container">
 	<div class="col-md-8">
-		<table class="table">
-			<thead>
-				<tr>
-					@foreach ($fields->headings() as $heading) <th>	{{ $heading }} </th> @endforeach
-				</tr>
-			</thead>
-			<tbody>
-			@foreach ($records as $record)
-				<tr data-url="{{ route("{$route}edit", $record->id) }}">
-					@foreach ($fields->values($record) as $item) <td>	{{ $item }} </td> @endforeach
-				</tr>
-			@endforeach
-			</tbody>
-		</table>
+
+		<div class="page-header">
+			<h1>{{ ucfirst($plural_name) }}</h1>
+			<div class="pull-right">
+				<a href="{{ route("{$route}create") }}" class="btn btn-primary">Create</a>
+			</div>
+		</div>
+
+		<div class="panel panel-default">
+			<table class="table">
+				<thead>
+					<tr>
+						@foreach ($fields->headings() as $heading) <th>	{{ $heading }} </th> @endforeach
+					</tr>
+				</thead>
+				<tbody>
+				@foreach ($records as $record)
+					<tr data-url="{{ route("{$route}edit", $record->id) }}">
+						@foreach ($fields->values($record) as $item) <td>	{{ $item }} </td> @endforeach
+					</tr>
+				@endforeach
+				</tbody>
+			</table>
+			<div class="panel-footer">
+
+			</div>
+		</div>
+
 	</div>
 </div>
 

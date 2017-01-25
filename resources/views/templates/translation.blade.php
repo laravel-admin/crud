7d22fd1e65bd433950217cee863e3a37dceb55aa
Marco Boom <marco@quest-media.nl>
2017-01-25 09:04:38 +0100

    Add template for translation

---
+++ resources/views/templates/translation.blade.php
@@ -0,0 +1,36 @@
+@extends($layout)
+
+@section('content')
+
+<div class="container">
+    <form action="{{ route("{$route}update", [$model->id, $translation]) }}" method="post" class="form-horizontal">
+        {!! csrf_field() !!}
+        {!! method_field('put') !!}
+
+        <div class="page-header">
+
+            <div class="pull-right">
+                <button type="submit" class="btn btn-primary">Save</button>
+
+            </div>
+
+            <h1>Edit {{ ucfirst($singular_name) }}</h1>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Settings</div>
+                    <div class="panel-body">
+                        @foreach ($fields->values() as $field)
+                            @if ($field && $view = $field->view()) @include($view, compact($field, $model)) @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+</div>
+
+@stop

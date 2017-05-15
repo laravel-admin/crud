5181f70981e18862ca385f4aba5d0049b4522b4b
Bas de Beer <bas@dekantvandebeer.nl>
2017-05-15 13:24:46 +0200

    Remove id from child options

--- resources/views/fields/select-parent-child-childs.blade.php
+++ resources/views/fields/select-parent-child-childs.blade.php
@@ -1,6 +1,6 @@
 @foreach($childs as $child)
     <option value="{{ $child->id }}" {{ $field->current_id($model) == $child->id ? 'disabled="disabled"' : '' }} {{ $field->value($model) == $child->id ? 'selected="selected"' : '' }}>
-        {{ $current_id . $prefix.$child->name }}
+        {{ $prefix.$child->name }}
     </option>
     @if(count($child->option_childs($current_id)))
         @include('crud::fields.select-parent-child-childs',['prefix' => $prefix . ' - ', 'childs' => $child->option_childs($current_id)])

490266d38f3356c625e7d6847659cc48910209fb
Bas de Beer <bas@dekantvandebeer.nl>
2017-05-15 12:25:22 +0200

    Update parent child select

--- resources/views/fields/select-parent-child-childs.blade.php
+++ resources/views/fields/select-parent-child-childs.blade.php
@@ -1,8 +1,8 @@
 @foreach($childs as $child)
-    <option value="{{ $option->id }}" {{ $field->value($model) == $child->id ? 'selected="selected"' : '' }}>
-        {{ $prefix.$child->name }}
+    <option value="{{ $child->id }}" {{ $field->current_id($model) == $child->id ? 'disabled="disabled"' : '' }} {{ $field->value($model) == $child->id ? 'selected="selected"' : '' }}>
+        {{ $current_id . $prefix.$child->name }}
     </option>
     @if(count($child->option_childs($current_id)))
-        @include('crud::fields.select-parent-child-childs',['prefix' => $prefix . '-> ', 'childs' => $child->option_childs($current_id)])
+        @include('crud::fields.select-parent-child-childs',['prefix' => $prefix . ' - ', 'childs' => $child->option_childs($current_id)])
     @endif
 @endforeach
--- resources/views/fields/select-parent-child.blade.php
+++ resources/views/fields/select-parent-child.blade.php
@@ -3,11 +3,11 @@
 	<div class="col-sm-9">
         <select name="{{ $field->id() }}" class="form-control {{ $field->css_classes() }}">
             @foreach ($field->options($model) as $option)
-                <option value="{{ $option->id }}" {{ $field->value($model) == $option->id ? 'selected="selected"' : '' }}>
+                <option value="{{ $option->id }}" {{ $field->current_id($model) == $option->id ? 'disabled="disabled"' : '' }} {{ $field->value($model) == $option->id ? 'selected="selected"' : '' }}>
                     {{ $option->name }}
                 </option>
                 @if(count($option->option_childs($field->current_id($model))))
-                    @include('crud::fields.select-parent-child-childs',['current_id' => $field->current_id($model), 'prefix' => '-> ', 'childs' => $option->option_childs($field->current_id($model))])
+                    @include('crud::fields.select-parent-child-childs',['current_id' => $field->current_id($model), 'prefix' => ' - ', 'childs' => $option->option_childs($field->current_id($model))])
                 @endif
             @endforeach
         </select>

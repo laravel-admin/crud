d22d175c1ae023d4774c188270b0a4eaec31dcc6
Bas de Beer <bas@dekantvandebeer.nl>
2017-06-08 17:00:18 +0200

    Update textarea template

--- resources/js/components/layout/fields/Textarea.vue
+++ resources/js/components/layout/fields/Textarea.vue
@@ -2,7 +2,7 @@
 	<div class="form-group">
 		<label :for="settings.id" class="col-sm-3 control-label">{{ settings.name }}</label>
 		<div class="col-sm-9">
-			<textarea :name="settings.id" class="form-control" :value="data" @input="$emit('update', settings.id, $event.target.value, index)"></textarea>
+			<textarea :name="settings.id" rows="4" class="form-control" :value="data" @input="$emit('update', settings.id, $event.target.value, index)"></textarea>
 		</div>
 	</div>
 </template>

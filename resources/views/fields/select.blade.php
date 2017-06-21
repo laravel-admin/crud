1050cf7972d4612fc6bde474f400434da242d123
Bas de Beer <bas@dekantvandebeer.nl>
2017-06-21 09:27:27 +0200

    Remember select field state

--- resources/views/fields/select.blade.php
+++ resources/views/fields/select.blade.php
@@ -3,7 +3,7 @@
 	<div class="col-sm-9">
 		<select name="{{ $field->id() }}" class="form-control {{ $field->css_classes() }}">
 			@foreach ($field->options($model) as $key=>$value)
-				<option value="{{ $key }}" {{ $field->value($model) == $key ? 'selected="selected"' : '' }}>
+				<option value="{{ $key }}" {{ ($field->value($model) == $key || old($field->id()) == $key) ? 'selected="selected"' : '' }}>
 					{{ $value }}
 				</option>
 			@endforeach

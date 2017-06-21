a8c8a0e33eae2081e053c17dd5ad678ddf4d4fe6
Bas de Beer <bas@dekantvandebeer.nl>
2017-06-21 09:27:03 +0200

    Remember boolean field state

--- resources/views/fields/boolean.blade.php
+++ resources/views/fields/boolean.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	<div class="col-sm-9 col-sm-push-3 checkbox">
 		<input type="hidden" name="{{ $field->id() }}" value="0" />
-		<label><input type="checkbox" name="{{ $field->id() }}" value="1" {{ $field->value($model) ? 'checked="checked"' : '' }}/> {{ $field->label() }}</label>
+		<label><input type="checkbox" name="{{ $field->id() }}" value="1" {{ ($field->value($model) || old($field->id())) ? 'checked="checked"' : '' }}/> {{ $field->label() }}</label>
     </div>
 </div>

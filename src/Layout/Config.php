400113d3ea90e88ced85bcdc3cb56acc2d9a86d5
Bas de Beer <bas@dekantvandebeer.nl>
2018-02-15 09:26:26 +0100

    Updated layout config setup for repeater
    Selectboxes in layout-repeater can now have callable options setup

--- src/Layout/Config.php
+++ src/Layout/Config.php
@@ -6,20 +6,25 @@ class Config
 {
     protected $data;
 
-    public function __construct(array $data=null)
+    public function __construct(array $data = null)
     {
         $this->data = $data ?: config('layout');
     }
-
 
     public function all()
     {
         $return = [];
 
-        foreach ($this->data['components'] as $key=>$value) {
-            foreach ($value['fields'] as $fieldId=>$field) {
+        foreach ($this->data['components'] as $key => $value) {
+            foreach ($value['fields'] as $fieldId => $field) {
                 if (isset($field['options']) && is_callable($field['options'])) {
                     $this->data['components'][$key]['fields'][$fieldId]['options'] = $field['options']();
+                } elseif ($field['type'] === 'layout-repeater') {
+                    foreach ($field['children'] as $childId => $field) {
+                        if (isset($field['options']) && is_callable($field['options'])) {
+                            $this->data['components'][$key]['fields'][$fieldId]['children'][$childId]['options'] = $field['options']();
+                        }
+                    }
                 }
             }
         }

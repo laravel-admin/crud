ad189ae70f24ca9658617ffdd6de08ede6f058f9
Jakob Buis <jakob.buis@in10.nl>
2018-08-07 16:09:35 +0200

    add extra check to make  work when empty

--- src/Traits/CanBeSecured.php
+++ src/Traits/CanBeSecured.php
@@ -18,6 +18,15 @@ trait CanBeSecured
     {
         $authorized = false;
 
+        // If there are no authorized roles specified ($authorizedRoles = [])
+        // we make an extra check for 'administrator', otherwise the gate facade
+        // is never triggered, and the before() check in laravel-admin/base fails.
+        if (count($this->authorizedRoles) === 0) {
+            if (Gate::allows('has-role', 'administrator')) {
+                $authorized = true;
+            }
+        }
+
         foreach ($this->authorizedRoles as $role) {
             if (Gate::allows('has-role', $role)) {
                 $authorized = true;

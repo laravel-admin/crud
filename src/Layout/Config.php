7a7a7d64c7dc830c222167792abfd546da08a4bf
Bas de Beer <bas@dekantvandebeer.nl>
2018-12-18 11:28:32 +0100

    Check if options for field selectbox has model

--- src/Layout/Config.php
+++ src/Layout/Config.php
@@ -17,12 +17,16 @@ class Config
 
         foreach ($this->data['components'] as $key => $value) {
             foreach ($value['fields'] as $fieldId => $field) {
-                if (isset($field['options']) && is_callable($field['options'])) {
+                if (isset($field['options']) && !is_array($field['options']) && is_callable($field['options'])) {
                     $this->data['components'][$key]['fields'][$fieldId]['options'] = $field['options']();
+                } elseif (isset($field['options']) && isset($field['options']['model']) && class_exists($field['options']['model'])) {
+                    $this->data['components'][$key]['fields'][$fieldId]['options'] = (new $field['options']['model'])::pluck($field['options']['value'], $field['options']['key'])->all();
                 } elseif ($field['type'] === 'layout-repeater') {
                     foreach ($field['children'] as $childId => $field) {
-                        if (isset($field['options']) && is_callable($field['options'])) {
+                        if (isset($field['options']) && !is_array($field['options']) && is_callable($field['options'])) {
                             $this->data['components'][$key]['fields'][$fieldId]['children'][$childId]['options'] = $field['options']();
+                        } elseif (isset($field['options']) && isset($field['options']['model']) && class_exists($field['options']['model'])) {
+                            $this->data['components'][$key]['fields'][$fieldId]['children'][$childId]['options'] = (new $field['options']['model'])::pluck($field['options']['value'], $field['options']['key'])->all();
                         }
                     }
                 }

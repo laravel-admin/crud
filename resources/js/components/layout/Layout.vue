899edcce4d84d58ddca1441c2e0d2c76da53bc03
Bas de Beer <bas@dekantvandebeer.nl>
2019-06-19 15:09:32 +0200

    Use import instead of require

--- resources/js/components/layout/Layout.vue
+++ resources/js/components/layout/Layout.vue
@@ -1,253 +1,239 @@
 <template>
+  <div v-if="settings">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <div class="btn-group pull-right" v-show="this.hastranslation=='true'">
+          <button
+            type="button"
+            class="btn btn-default btn-xs dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Add
+            <span class="caret"></span>
+          </button>
 
-    <div v-if="settings">
+          <ul class="dropdown-menu">
+            <li v-for="component in settings.components">
+              <a href="#" class v-on:click.prevent="appendComponent(component)">{{ component.name }}</a>
+            </li>
+          </ul>
+        </div>
 
-		<div class="panel panel-default">
+        <h3 class="panel-title">{{ this.language }} {{ this.title }}</h3>
+      </div>
 
-			<div class="panel-heading">
-                
-				<div class="btn-group pull-right" v-show="this.hastranslation=='true'">
-                    
-					<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Add <span class="caret"></span>
-					</button>
+      <div class="panel-body" v-show="this.hastranslation=='false'">
+        <p>Please use the content setup for the selected language first.</p>
+      </div>
 
-					<ul class="dropdown-menu">
-						<li v-for="component in settings.components"><a href="#" class="" v-on:click.prevent="appendComponent(component)">{{ component.name }}</a></li>
-					</ul>
-
-                </div>
-
-				<h3 class="panel-title">{{ this.language }} {{ this.title }}</h3>
-
-            </div>
-
-			<div class="panel-body" v-show="this.hastranslation=='false'">
-				
-				<p>Please use the content setup for the selected language first.</p>
-
-			</div>
-
-			<div class="panel-body" v-show="this.hastranslation=='true'">
-
-				<layout-component
-					v-for="(component,key) in checkedData"
-					:key="key"
-					:index="key"
-					:data="component"
-					:locale="locale"
-					:length="checkedData.length"
-					:components="settings.components"
-					:settings="getSettingsForComponent(component.settings.type)"
-					@update="updateComponent"
-					@append="appendComponent"
-					@delete="deleteComponent"
-					@moveup="moveComponentUp"
-					@movedown="moveComponentDown"
-				></layout-component>
-
-			</div>
-
-		</div>
-
+      <div class="panel-body" v-show="this.hastranslation=='true'">
+        <layout-component
+          v-for="(component,key) in checkedData"
+          :key="key"
+          :index="key"
+          :data="component"
+          :locale="locale"
+          :length="checkedData.length"
+          :components="settings.components"
+          :settings="getSettingsForComponent(component.settings.type)"
+          @update="updateComponent"
+          @append="appendComponent"
+          @delete="deleteComponent"
+          @moveup="moveComponentUp"
+          @movedown="moveComponentDown"
+        ></layout-component>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
-	import Event from '../../../../../base/resources/js/Event';
+import Event from '../../../../../base/resources/js/Event';
+import Component from './Component.vue';
 
-    export default {
+export default {
+    components: {
+        'layout-component': Component,
+    },
 
-		components: {
-			'layout-component': require('./Component.vue'),
+    props: ['title', 'layoutdata', 'layoutsettings', 'hastranslation', 'language', 'locale', 'controller'],
+
+    data() {
+        let output = {
+            //	Initialise settings object, who will be filled by an ajax request
+            settings: this.layoutsettings,
+
+            //	The data of the current object
+            data: this.addWatcherIndexToLayoutData(),
+        };
+
+        return output;
+    },
+
+    mounted() {
+        console.log('Layout component mounted.');
+    },
+
+    computed: {
+        checkedData() {
+            let output = this.data.filter(item => {
+                if (typeof item.settings == 'undefined') {
+                    return false;
+                }
+
+                if (typeof item.settings.type == 'undefined') {
+                    return false;
+                }
+
+                return this.getSettingsForComponent(item.settings.type) ? true : false;
+            });
+
+            return output;
+        },
+    },
+
+    methods: {
+        generateUniqueId(val) {
+            return (Math.random() * (val + 1)).toString(36).substr(2, 16);
         },
 
-		props: ['title', 'layoutdata', 'layoutsettings', 'hastranslation', 'language', 'locale', 'controller'],
+        addWatcherIndexToLayoutData() {
+            let settings = this.layoutdata;
 
-		data()
-		{
-			let output = {
-				//	Initialise settings object, who will be filled by an ajax request
-				settings: this.layoutsettings,
+            settings.forEach((item, index) => {
+                settings[index].watcher_index = this.generateUniqueId(index);
+            });
 
-				//	The data of the current object
-				data: this.addWatcherIndexToLayoutData(),
-			};
-
-			return output;
-		},
-
-        mounted() {
-            console.log('Layout component mounted.');
+            return settings;
         },
 
-		computed: {
+        /**
+         * Append a component to the layout
+         * @param object component
+         * @param int index
+         */
+        appendComponent(component, index) {
+            //	Prototype for the new data object
+            let obj = {
+                settings: {
+                    name: component.name,
+                    active: true,
+                    type: component.id,
+                },
+                watcher_index: this.generateUniqueId(this.checkedData.length),
+            };
 
-            checkedData()
-            {
-                let output = this.data.filter(item => {
-                    if (typeof item.settings == 'undefined') {
-                        return false;
-					}
-					
-                    if (typeof item.settings.type == 'undefined') {
-                        return false;
-					}
-					
-					return this.getSettingsForComponent(item.settings.type) ? true : false;
-				});
+            this.data.push(obj);
 
-				return output;
+            // TODO: Append new component to the specified index
+
+            //	If no index specified, append the component to the bottom
+            //if (typeof index === 'undefined') this.data.push(obj);
+
+            //	Append the component at the specified position
+            //else this.data.splice(index, 0, obj);
+        },
+
+        /**
+         * Delete a component from the layout
+         * @param int index
+         */
+        deleteComponent(index) {
+            //	Delete the component with the given index of the array
+            this.data.splice(index, 1);
+        },
+
+        moveComponentDown(index) {
+            if (typeof this.data[index + 1] == 'undefined') {
+                return;
             }
 
-		},
+            let item = this.data[index];
 
-		methods:
-		{
-			generateUniqueId(val){
-				return (Math.random() * (val+1)).toString(36).substr(2, 16);
-			},
+            this.data.splice(index, 1);
+            this.data.splice(index + 1, 0, item);
+        },
 
-			addWatcherIndexToLayoutData() {
-				let settings = this.layoutdata;
-				
-				settings.forEach((item, index) => {
-					settings[index].watcher_index = this.generateUniqueId(index);
-				});
+        moveComponentUp(index) {
+            if (typeof this.data[index - 1] == 'undefined') {
+                return;
+            }
 
-				return settings;
-			},
+            let item = this.data[index];
 
-			/**
-			 * Append a component to the layout
-			 * @param object component
-			 * @param int index
-			 */
-			appendComponent(component, index)
-			{
-				//	Prototype for the new data object
-				let obj = {
-					settings: {
-						name: component.name,
-						active: true,
-						type: component.id,
-					},
-					watcher_index: this.generateUniqueId(this.checkedData.length)
-				};
+            this.data.splice(index, 1);
+            this.data.splice(index - 1, 0, item);
+        },
 
-				this.data.push(obj);
+        /**
+         * Get the settings for the component wich has to be rendered
+         * @param string type
+         */
+        getSettingsForComponent(type) {
+            //	Search in all defined componenttypes for the one with an id as type
+            for (let i in this.settings.components) {
+                if (this.settings.components[i].id == type) {
+                    return this.settings.components[i];
+                }
+            }
 
-				// TODO: Append new component to the specified index
+            //	Return null if component type is not found
+            return null;
+        },
 
-				//	If no index specified, append the component to the bottom
-				//if (typeof index === 'undefined') this.data.push(obj);
+        /**
+         * Update a single value of the content of settings of a particular component
+         * @param int index
+         * @param string section
+         * @param string field
+         * @param mixed data
+         */
+        updateComponent(index, section, field, data) {
+            //	Does a component with the specified index exists?
+            if (typeof this.data[index] === 'undefined') this.data[index] = {};
 
-				//	Append the component at the specified position
-				//else this.data.splice(index, 0, obj);
-			},
+            //	Does the section (content or setttings) exists on the component
+            if (typeof this.data[index][section] === 'undefined') this.data[index][section] = {};
 
-			/**
-			 * Delete a component from the layout
-			 * @param int index
-			 */
-			deleteComponent(index)
-			{
-				//	Delete the component with the given index of the array
-				this.data.splice(index,1);
-			},
+            //	Does the field exists on the component
+            if (typeof this.data[index][section][field] === 'undefined') this.data[index][section][field] = {};
 
-			moveComponentDown(index)
-			{
-				if (typeof this.data[index+1] == 'undefined') {
-					return;
-				}
+            //	Update the data of the field
+            this.data[index][section][field] = data;
+        },
 
-				let item = this.data[index];
-
-				this.data.splice(index,1);
-				this.data.splice(index+1, 0, item);
-			},
-
-			moveComponentUp(index)
-			{
-				if (typeof this.data[index-1] == 'undefined') {
-					return;
-				}
-
-				let item = this.data[index];
-
-				this.data.splice(index,1);
-				this.data.splice(index-1, 0, item);
-			},
-
-			/**
-			 * Get the settings for the component wich has to be rendered
-			 * @param string type
-			 */
-			getSettingsForComponent(type)
-			{
-				//	Search in all defined componenttypes for the one with an id as type
-				for (let i in this.settings.components)
-				{
-					if (this.settings.components[i].id == type)
-					{
-						return this.settings.components[i];
-					}
-				}
-
-				//	Return null if component type is not found
-				return null;
-			},
-
-			/**
-			 * Update a single value of the content of settings of a particular component
-			 * @param int index
-			 * @param string section
-			 * @param string field
-			 * @param mixed data
-			 */
-			updateComponent(index, section, field, data)
-			{
-				//	Does a component with the specified index exists?
-				if (typeof this.data[index] === 'undefined') this.data[index] = {};
-
-				//	Does the section (content or setttings) exists on the component
-				if (typeof this.data[index][section] === 'undefined') this.data[index][section] = {};
-				
-				//	Does the field exists on the component
-				if (typeof this.data[index][section][field] === 'undefined') this.data[index][section][field] = {};
-
-				//	Update the data of the field
-				this.data[index][section][field] = data;
-			},
-
-			save()
-			{
-                const notify = () => {
-                    Event.$emit('notification', {type: 'success', message: 'The layout is succesfully saved.'});
-                };
-				const flash_error = (data) => {
-					let message = data.message;
-					for (var key in data.errors) {
-						if (data.errors.hasOwnProperty(key)) {
-							message = data.errors[key].toString();
-							break;
-						}
-					}
-					Event.$emit('notification', {type: 'danger', message: message});
-				};
-				if (this.locale) {
-					axios.put(this.controller, {layout:this.checkedData}).then(notify).catch(error => {
-						flash_error(error.response.data);
-					});
-				}
-				else {
-					axios.post(this.controller, {layout:this.checkedData}).then(notify).catch(error => {
-						flash_error(error.response.data);
-					});
-				}
-			}
-		}
-    }
+        save() {
+            const notify = () => {
+                Event.$emit('notification', { type: 'success', message: 'The layout is succesfully saved.' });
+            };
+            const flash_error = data => {
+                let message = data.message;
+                for (var key in data.errors) {
+                    if (data.errors.hasOwnProperty(key)) {
+                        message = data.errors[key].toString();
+                        break;
+                    }
+                }
+                Event.$emit('notification', { type: 'danger', message: message });
+            };
+            if (this.locale) {
+                axios
+                    .put(this.controller, { layout: this.checkedData })
+                    .then(notify)
+                    .catch(error => {
+                        flash_error(error.response.data);
+                    });
+            } else {
+                axios
+                    .post(this.controller, { layout: this.checkedData })
+                    .then(notify)
+                    .catch(error => {
+                        flash_error(error.response.data);
+                    });
+            }
+        },
+    },
+};
 </script>
--- resources/js/components/layout/NestedComponent.vue
+++ resources/js/components/layout/NestedComponent.vue
@@ -1,138 +1,171 @@
 <template>
-    
-	<div class="panel panel-default">
-	
-		<div class="panel-heading">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <div class="btn-group pull-right">
+        <button
+          type="button"
+          class="btn btn-default btn-xs"
+          :class="{'active':view=='content'}"
+          @click.prevent="setView('content')"
+        >Content</button>
 
-			<div class="btn-group pull-right">
-				
-				<button type="button" class="btn btn-default btn-xs" :class="{'active':view=='content'}" @click.prevent="setView('content')">
-					Content
-				</button>
+        <button
+          type="button"
+          class="btn btn-default btn-xs"
+          :class="{'active':view=='settings'}"
+          @click.prevent="setView('settings')"
+        >Settings</button>
 
-				<button type="button" class="btn btn-default btn-xs" :class="{'active':view=='settings'}" @click.prevent="setView('settings')">
-					Settings
-				</button>
+        <div class="btn-group">
+          <button
+            type="button"
+            class="btn btn-default btn-xs dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Action
+            <span class="caret"></span>
+          </button>
 
-				<div class="btn-group">
-					
-					<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Action <span class="caret"></span>
-					</button>
-
-					<ul class="dropdown-menu">
-						<li v-if="index"><a href="#" class="" @click.prevent="$emit('moveup', index)">Move up</a></li>
-						<li v-if="index < length-1"><a href="#" class="" @click.prevent="$emit('movedown', index)">Move down</a></li>
-						<li><a href="#" class="" @click.prevent="$emit('delete', index)">Delete</a></li>
-						<li role="separator" class="divider"></li>
-						<li v-for="component in components"><a href="#"  v-on:click.prevent="$emit('append', component, index)">Add {{ component.name }}</a></li>
-					</ul>
-
-				</div>
-
-			</div>
-
-			<h3 class="panel-title">{{ data.settings.name }}</h3>
-
+          <ul class="dropdown-menu">
+            <li v-if="index">
+              <a href="#" class @click.prevent="$emit('moveup', index)">Move up</a>
+            </li>
+            <li v-if="index < length-1">
+              <a href="#" class @click.prevent="$emit('movedown', index)">Move down</a>
+            </li>
+            <li>
+              <a href="#" class @click.prevent="$emit('delete', index)">Delete</a>
+            </li>
+            <li role="separator" class="divider"></li>
+            <li v-for="component in components">
+              <a
+                href="#"
+                v-on:click.prevent="$emit('append', component, index)"
+              >Add {{ component.name }}</a>
+            </li>
+          </ul>
         </div>
+      </div>
 
-		<div class="panel-body" v-show="view == 'content'">
-			<template v-for="(field, key) in settings.fields">
-				<component :is="field.type" :settings="field" :data="getDataForField(field.id)" :watcher_index="data.watcher_index" :key="generateUniqueId('content', key)" :index="index" @update="updateContentField"></component>
-			</template>
-		</div>
-
-		<div class="panel-body" v-show="view == 'settings'">
-			<template v-for="(field, key) in componentSettings">
-				<component :is="field.type" :settings="field" :data="data.settings[field.id]" :watcher_index="data.watcher_index" :key="generateUniqueId('setting', key)" :index="index" @update="updateSettingsField"></component>
-			</template>
-		</div>		
-
+      <h3 class="panel-title">{{ data.settings.name }}</h3>
     </div>
+
+    <div class="panel-body" v-show="view == 'content'">
+      <template v-for="(field, key) in settings.fields">
+        <component
+          :is="field.type"
+          :settings="field"
+          :data="getDataForField(field.id)"
+          :watcher_index="data.watcher_index"
+          :key="generateUniqueId('content', key)"
+          :index="index"
+          @update="updateContentField"
+        ></component>
+      </template>
+    </div>
+
+    <div class="panel-body" v-show="view == 'settings'">
+      <template v-for="(field, key) in componentSettings">
+        <component
+          :is="field.type"
+          :settings="field"
+          :data="data.settings[field.id]"
+          :watcher_index="data.watcher_index"
+          :key="generateUniqueId('setting', key)"
+          :index="index"
+          @update="updateSettingsField"
+        ></component>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-		//	Load all field types
-		components: {
-	        'layout-boolean': require('./fields/Boolean.vue'),
-			'layout-date': require('./fields/Date.vue'),
-	        'layout-media-item': require('./fields/MediaItem.vue'),
-	        'layout-repeater': require('./fields/Repeater.vue'),
-	        'layout-select': require('./fields/Select.vue'),
-			'layout-text': require('./fields/Text.vue'),
-	        'layout-textarea': require('./fields/Textarea.vue'),
-	        'layout-wysiwyg': require('./fields/Wysiwyg.vue'),
-		},
+import FieldBoolean from './fields/Boolean.vue';
+import FieldDate from './fields/Date.vue';
+import FieldMediaItem from './fields/MediaItem.vue';
+import FieldRepeater from './fields/Repeater.vue';
+import FieldSelect from './fields/Select.vue';
+import FieldText from './fields/Text.vue';
+import FieldTextarea from './fields/Textarea.vue';
+import FieldWysiwyg from './fields/Wysiwyg.vue';
 
-		//	Define the props, given from the layout component
-		props: ['data','settings','index','length', 'components'],
+export default {
+    //	Load all field types
+    components: {
+        'layout-boolean': FieldBoolean,
+        'layout-date': FieldDate,
+        'layout-media-item': FieldMediaItem,
+        'layout-repeater': FieldRepeater,
+        'layout-select': FieldSelect,
+        'layout-text': FieldText,
+        'layout-textarea': FieldTextarea,
+        'layout-wysiwyg': FieldWysiwyg,
+    },
 
-		data()
-		{
-			return {
-				//	Current view
-				view:null,
+    //	Define the props, given from the layout component
+    props: ['data', 'settings', 'index', 'length', 'components'],
 
-				//	The default settings for each component
-				componentSettings: [
-					{id:'name', name:'Name', type:'layout-text'},
-				]
-			};
-		},
+    data() {
+        return {
+            //	Current view
+            view: null,
 
-		methods: {
-			/**
-			 * Create component unique id
-			 * @param string type
-			 * @param int key
-			 */
-			generateUniqueId(type, key){
-				return type + key + this.data.watcher_index;
-			},
+            //	The default settings for each component
+            componentSettings: [{ id: 'name', name: 'Name', type: 'layout-text' }],
+        };
+    },
 
-			/**
-			 * Toggle the view of a component between content, settings and blank
-			 * @param string view
-			 */
-			setView(view)
-			{
-				this.view = (this.view == view) ? null : view;
-			},
+    methods: {
+        /**
+         * Create component unique id
+         * @param string type
+         * @param int key
+         */
+        generateUniqueId(type, key) {
+            return type + key + this.data.watcher_index;
+        },
 
-			/**
-			 * Check if the data object has already has a property for the specific field
-			 * @param string id
-			 */
-			getDataForField(id)
-			{
-				if (typeof this.data.content === 'undefined') return null;
-				if (typeof this.data.content[id] === 'undefined') return null;
+        /**
+         * Toggle the view of a component between content, settings and blank
+         * @param string view
+         */
+        setView(view) {
+            this.view = this.view == view ? null : view;
+        },
 
-				return this.data.content[id];
-			},
+        /**
+         * Check if the data object has already has a property for the specific field
+         * @param string id
+         */
+        getDataForField(id) {
+            if (typeof this.data.content === 'undefined') return null;
+            if (typeof this.data.content[id] === 'undefined') return null;
 
-			/**
-			 * Update the value of the by id given content field
-			 * @param string id
-			 * @param mixed data
-			 */
-			updateContentField(id, data)
-			{
-				//	Broadcast an event to the layout to update the data in the original object
-				this.$emit('update', this.index, 'content', id, data);
-			},
+            return this.data.content[id];
+        },
 
-			/**
-			 * Update the value of the by id given settings field
-			 * @param string id
-			 * @param mixed data
-			 */
-			updateSettingsField(id, data)
-			{
-				//	Broadcast an event to the layout to update the data in the original object
-				this.$emit('update', this.index, 'settings', id, data);
-			}
-		}
-    }
+        /**
+         * Update the value of the by id given content field
+         * @param string id
+         * @param mixed data
+         */
+        updateContentField(id, data) {
+            //	Broadcast an event to the layout to update the data in the original object
+            this.$emit('update', this.index, 'content', id, data);
+        },
+
+        /**
+         * Update the value of the by id given settings field
+         * @param string id
+         * @param mixed data
+         */
+        updateSettingsField(id, data) {
+            //	Broadcast an event to the layout to update the data in the original object
+            this.$emit('update', this.index, 'settings', id, data);
+        },
+    },
+};
 </script>

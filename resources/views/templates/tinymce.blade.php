9e9b4efdea1d2188d2a488db512ac0e2dc93375f
Bas de Beer <bas@dekantvandebeer.nl>
2021-01-15 12:22:32 +0100

    Updated script src

--- resources/views/templates/tinymce.blade.php
+++ resources/views/templates/tinymce.blade.php
@@ -1,3 +1,3 @@
 @push('scripts_before')
-	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+	<script src="//cdn.tiny.cloud/1/{{config('tiny_editor.api_key')}}/tinymce/5/tinymce.min.js"></script>
 @endpush
